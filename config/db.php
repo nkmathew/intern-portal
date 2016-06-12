fa7eec16c2e3bca3377a572b8184a1c1fcefd8b0
nkmathew <kipkoechmathew@gmail.com>
2016-06-12 21:37:59 +0300

    Set db name

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2_basic_with_db',
+    'dsn' => 'mysql:host=localhost;dbname=intern_system',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
