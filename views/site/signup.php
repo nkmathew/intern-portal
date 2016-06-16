6c2a28c38ea9abbe683495869a4d56affa73ec93
nkmathew <kipkoechmathew@gmail.com>
2016-06-16 11:33:51 +0300

    Add signup instructions

--- views/site/signup.php
+++ views/site/signup.php
@@ -13,7 +13,8 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <div class="site-signup">
     <h1><?= Html::encode($this->title) ?></h1>
-    <p>Please fill out the following fields to signup:</p>
+    <p>Please fill out the following fields to signup if you are either a supervisor or coordinator</p>
+    <p>Student interns can only sign up on invite from their coordinators and supervisors</p>
     <div class="row">
         <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
