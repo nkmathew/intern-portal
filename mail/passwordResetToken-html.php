8369f4d4f69142c4d457ec6cad85521f63905e1b
nkmathew <kipkoechmathew@gmail.com>
2016-06-16 11:49:57 +0300

    Remove username field in password reset

--- mail/passwordResetToken-html.php
+++ mail/passwordResetToken-html.php
@@ -8,7 +8,7 @@ $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 't
 ?>
 
 <div class="password-reset">
-    <p>Hello <?= Html::encode($user->username) ?>,</p>
+    <p>Hello <?= Html::encode($user->email) ?>,</p>
     <p>Follow the link below to reset your password:</p>
     <p><?= Html::a(Html::encode($resetLink), $resetLink) ?></p>
 </div>
--- mail/passwordResetToken-text.php
+++ mail/passwordResetToken-text.php
@@ -6,6 +6,6 @@
 $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
 ?>
 
-Hello <?= $user->username ?>,
+Hello <?= $user->email ?>,
 Follow the link below to reset your password:
 <?= $resetLink ?>
