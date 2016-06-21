ca05969c395a13f8ff43fbdb300dd0cb5c34efa1
nkmathew <kipkoechmathew@gmail.com>
2016-06-21 14:53:23 +0300

    Remove unused css class

---
+++ views/site/profile.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @project: Intern Portal.
+ * @author: nkmathew
+ * @date: 20/06/2016
+ * @time: 17:09
+ */
+ 
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'First profile tab';
+?>
+
+<div>
+
+    <?= Html::a('Pjax Link tester', ['update', 'id' => '#s'], ['data-pjax'=> '#formsection']) ?>
+    <p>This is some cool stuff
+    </p>
+</div>
