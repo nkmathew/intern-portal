7763275f2de8a7b4d9abf0030c6af92e92f0b049
nkmathew <kipkoechmathew@gmail.com>
2016-06-22 11:02:28 +0300

    Validate with ajax for all active forms

--- views/site/resetPassword.php
+++ views/site/resetPassword.php
@@ -17,7 +17,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>Please choose your new password:</p>
     <div class="row">
         <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
+            <?php $form = ActiveForm::begin(['id' => 'reset-password-form', 'enableAjaxValidation' => true]); ?>
                 <?= $form->field($model, 'password')->passwordInput(['autofocus' => true]) ?>
                 <div class="form-group">
                     <?= Html::submitButton('Save', ['class' => 'btn btn-primary', 'id' => 'link-pwd-reset-btn']) ?>
