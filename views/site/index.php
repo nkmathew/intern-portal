6ea88e321828494d361ee122937fba4832896f32
nkmathew <kipkoechmathew@gmail.com>
2016-06-22 13:03:30 +0300

    Add tab for filling logbook

--- views/site/index.php
+++ views/site/index.php
@@ -4,6 +4,7 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\Tabs;
+use app\models\ProfileForm;
 
 $this->title = 'Home';
 ?>
@@ -15,13 +16,20 @@ Tabs::widget([
     'items' => [
         [
             'label' => 'Profile',
-            'content' => $this->render('profile'),
+            'content' => $this->render('profile', ['model' => new ProfileForm()]),
+            'headerOptions' => ['id' => 'profile-tab'],
             'active' => true
+        ],
+        [
+            'label' => 'Log Book',
+            'content' => $this->render('logbook'),
+            'headerOptions' => ['id' => 'logbook-tab'],
+            'options' => ['id' => 'tab-logbook'],
         ],
         [
             'label' => 'Notifications',
             'content' => $this->render('notifications'),
-            'headerOptions' => [],
+            'headerOptions' => ['id' => 'notifications-tab'],
             'options' => ['id' => 'tab-notifications'],
         ],
     ],
