2b46ec06b761392571a621f19878893f5a43be75
nkmathew <kipkoechmathew@gmail.com>
2016-06-22 16:42:34 +0300

    Add glyph icons in tab widget headers

--- views/site/index.php
+++ views/site/index.php
@@ -2,37 +2,37 @@
 
 /* @var $this yii\web\View */
 
-use yii\helpers\Html;
-use yii\bootstrap\Tabs;
 use app\models\ProfileForm;
+use yii\bootstrap\Tabs;
 
 $this->title = 'Home';
 ?>
 
 <title>Home</title> <!-- For Pjax's sake -->
 <div class="site-index">
-<?=
-Tabs::widget([
-    'items' => [
-        [
-            'label' => 'Profile',
-            'content' => $this->render('profile', ['model' => new ProfileForm()]),
-            'headerOptions' => ['id' => 'profile-tab'],
-            'active' => true
+    <?=
+    Tabs::widget([
+        'encodeLabels' => false,
+        'items' => [
+            [
+                'label' => '<span class="glyphicon glyphicon-user"></span> Profile',
+                'content' => $this->render('profile', ['model' => new ProfileForm()]),
+                'headerOptions' => ['id' => 'profile-tab'],
+                'active' => true
+            ],
+            [
+                'label' => '<span class="glyphicon glyphicon-book"></span> Log Book',
+                'content' => $this->render('logbook'),
+                'headerOptions' => ['id' => 'logbook-tab'],
+                'options' => ['id' => 'tab-logbook'],
+            ],
+            [
+                'label' => '<span class="glyphicon glyphicon-inbox"></span> Notifications',
+                'content' => $this->render('notifications'),
+                'headerOptions' => ['id' => 'notifications-tab'],
+                'options' => ['id' => 'tab-notifications'],
+            ],
         ],
-        [
-            'label' => 'Log Book',
-            'content' => $this->render('logbook'),
-            'headerOptions' => ['id' => 'logbook-tab'],
-            'options' => ['id' => 'tab-logbook'],
-        ],
-        [
-            'label' => 'Notifications',
-            'content' => $this->render('notifications'),
-            'headerOptions' => ['id' => 'notifications-tab'],
-            'options' => ['id' => 'tab-notifications'],
-        ],
-    ],
-]);
-?>
+    ]);
+    ?>
 </div>
