e621a39301e7cd8315bcc041f23861d1417b6909
nkmathew <kipkoechmathew@gmail.com>
2016-06-22 21:45:14 +0300

    Change database name

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=intern_system',
+    'dsn' => 'mysql:host=localhost;dbname=intern_portal',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
