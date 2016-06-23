c8130124ae71b8e770a0a3044ab36bfbc2795615
nkmathew <kipkoechmathew@gmail.com>
2016-06-23 15:01:36 +0300

    Stub for logbook tab

---
+++ views/site/logbook.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @project: Intern Portal.
+ * @author: nkmathew
+ * @date: 22/06/2016
+ * @time: 12:58
+ */
+ 
+/* @var $this yii\web\View */
+
+?>
+
+<div id="site-logbook">
+    <p>Logbook section...</p>
+</div>
