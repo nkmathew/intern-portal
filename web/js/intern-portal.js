51111405e7e8074459c42425f299cdd9d689c177
nkmathew <kipkoechmathew@gmail.com>
2016-06-24 01:50:28 +0300

    Logic for deleting and adding new list items

--- web/js/intern-portal.js
+++ web/js/intern-portal.js
@@ -8,4 +8,16 @@ $(document).ready(function () {
 
     var html = template({email: 'ngetich.kipkoech@students.jkuat.ac.ke'});
     $('#email-list-section').append(html);
+
+    $("#email-input-box").keyup(function (e) {
+        if (e.keyCode == 13) {
+            var email = $('#email-input-box').val() + '@students.jkuat.ac.ke';
+            var html = template({email: email});
+            $('#email-list-section').append(html);
+        }
+    });
+
+    $('.email-delete-btn').click(function () {
+        $(this).closest('.email-line').remove();
+    })
 });
