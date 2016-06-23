e8c3ab8d9c309506680ef21acb04ef32fec09416
nkmathew <kipkoechmathew@gmail.com>
2016-06-23 15:29:45 +0300

    Add coordinator's console

--- views/site/index.php
+++ views/site/index.php
@@ -10,29 +10,35 @@ $this->title = 'Home';
 
 <title>Home</title> <!-- For Pjax's sake -->
 <div class="site-index">
-    <?=
-    Tabs::widget([
-        'encodeLabels' => false,
-        'items' => [
-            [
-                'label' => '<span class="glyphicon glyphicon-user"></span> Profile',
-                'content' => $this->render('profile', ['model' => new ProfileForm()]),
-                'headerOptions' => ['id' => 'profile-tab'],
-                'active' => true
-            ],
-            [
-                'label' => '<span class="glyphicon glyphicon-book"></span> Log Book',
-                'content' => $this->render('logbook'),
-                'headerOptions' => ['id' => 'logbook-tab'],
-                'options' => ['id' => 'tab-logbook'],
-            ],
-            [
-                'label' => '<span class="glyphicon glyphicon-inbox"></span> Notifications',
-                'content' => $this->render('notifications'),
-                'headerOptions' => ['id' => 'notifications-tab'],
-                'options' => ['id' => 'tab-notifications'],
-            ],
+    <?php
+    $tabItems = [
+        [
+            'label' => '<span class="glyphicon glyphicon-user"></span> Profile',
+            'content' => $this->render('profile', ['model' => new ProfileForm()]),
+            'headerOptions' => ['id' => 'profile-tab'],
+            'active' => true
         ],
-    ]);
+        [
+            'label' => '<span class="glyphicon glyphicon-book"></span> Log Book',
+            'content' => $this->render('logbook'),
+            'headerOptions' => ['id' => 'logbook-tab'],
+            'options' => ['id' => 'tab-logbook'],
+        ],
+        [
+            'label' => '<span class="glyphicon glyphicon-inbox"></span> Notifications',
+            'content' => $this->render('notifications'),
+            'headerOptions' => ['id' => 'notifications-tab'],
+            'options' => ['id' => 'tab-notifications'],
+        ],
+    ];
+    if (!Yii::$app->user->isGuest) {
+        $tabItems[] = [
+            'label' => '<span class="glyphicon glyphicon-console"></span> Coordinator\'s Console',
+            'content' => $this->render('logbook'),
+            'headerOptions' => ['id' => 'logbook-tab'],
+            'options' => ['id' => 'tab-logbook'],
+        ];
+    }
+    echo Tabs::widget(['encodeLabels' => false, 'items' => $tabItems]);
     ?>
 </div>
