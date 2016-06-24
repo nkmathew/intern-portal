687cec4e596af4f198ff060377d727e6c7f1bf4a
nkmathew <kipkoechmathew@gmail.com>
2016-06-24 06:34:15 +0300

    Change get attribute

--- mail/signupLink-text.php
+++ mail/signupLink-text.php
@@ -3,7 +3,7 @@
 /* @var $this yii\web\View */
 /* @var $signupLink app\models\User */
 
-$signupUrl = Yii::$app->urlManager->createAbsoluteUrl(['site/signup-thru-link', 'token' => $signupLink->signup_token]);
+$signupUrl = Yii::$app->urlManager->createAbsoluteUrl(['site/signup-thru-link', 'signup_token' => $signupLink->signup_token]);
 ?>
 
 Hello <?= $signupLink->email ?>,
