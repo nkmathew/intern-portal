c17e64f84cc2d3ecf88d383960bde316ece8a864
nkmathew <kipkoechmathew@gmail.com>
2016-06-24 10:38:07 +0300

    Implement link signup

---
+++ views/site/linkSignup.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @project: Intern Portal.
+ * @author: nkmathew
+ * @date: 24/06/2016
+ * @time: 09:02
+ */
+ 
+/* @var $this yii\web\View */
+
+use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
+
+$this->title = 'Link Signup';
+?>
+
+<div class="site-link-signup">
+    <h3>Student intern link signup</h3>
+    <div class="row">
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin(['id' => 'form-signup', 'action' => '/site/signup']); ?>
+            <?= $form->field($model, 'email')->textInput(['readonly' => true, 'value' => $email]) ?>
+            <?= $form->field($model, 'password')->passwordInput() ?>
+            <div class="form-group">
+                <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
+            </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+</div>
