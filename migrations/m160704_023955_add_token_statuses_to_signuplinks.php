3d8a752e1aa5a74f4d3272f2519104edb5d89969
nkmathew <kipkoechmathew@gmail.com>
2016-07-03 12:32:27 +0300

    Create fields to track signup token usage

---
+++ migrations/m160704_023955_add_token_statuses_to_signuplinks.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles adding token_statuses to table `signuplinks`.
+ */
+class m160704_023955_add_token_statuses_to_signuplinks extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        // There can be only one used token in this table
+        $this->addColumn('signuplinks', 'token_used', $this->boolean());
+        // A token is disabled when the user signs up using some other token
+        $this->addColumn('signuplinks', 'token_disabled', $this->boolean());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropColumn('signuplinks', 'token_used');
+        $this->dropColumn('signuplinks', 'token_disabled');
+    }
+}
