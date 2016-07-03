c9500be7353aeb77ed037bcb11e60e595bd632b5
nkmathew <kipkoechmathew@gmail.com>
2016-07-03 12:32:27 +0300

    Update tab link urls

--- views/site/coordinatorConsole.php
+++ views/site/coordinatorConsole.php
@@ -9,17 +9,17 @@
     <div class="col-xs-2"> <!-- required for floating -->
         <!-- Nav tabs -->
         <ul class="nav nav-tabs tabs-left">
-            <li class="active"><a href="#home" data-toggle="tab">Send Signup invites</a></li>
-            <li><a href="#profile" data-toggle="tab">Signup Invite List</a></li>
+            <li class="active"><a href="#tab-invite-student" data-toggle="tab">Invite student</a></li>
+            <li><a href="#tab-sent-invites" data-toggle="tab">Sent invites</a></li>
         </ul>
     </div>
     <div class="col-xs-9">
         <!-- Tab panes -->
         <div class="tab-content">
-            <div class="tab-pane active" id="home">
+            <div class="tab-pane active" id="tab-invite-student">
                 <?= $this->render('signupInvites'); ?>
             </div>
-            <div class="tab-pane" id="profile">Profile Tab.</div>
+            <div class="tab-pane" id="tab-sent-invites">List of sent invites</div>
         </div>
     </div>
 </div>
