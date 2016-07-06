714b653397b925dd2038df1ae5b284c6d4a2f84f
nkmathew <kipkoechmathew@gmail.com>
2016-07-06 15:30:02 +0300

    Add panel header for profile details form and disabled cursor for email field

--- views/site/profile.php
+++ views/site/profile.php
@@ -19,24 +19,30 @@ ProfileAsset::register($this);
 ?>
 
 <div class="site-profile">
-    <p>
-        Update your profile below
-    </p>
     <div class="row">
-        <div class="col-lg-5">
+        <div class="col-lg-6">
             <?php $form = ActiveForm::begin([
                 'action' => '/site/profile',
                 'id' => 'profile-form',
                 'validateOnSubmit' => true,
                 'validateOnBlur' => true,
             ]); ?>
-            <?= $form->field($model, 'firstName')->textInput(['autofocus' => true, 'value' => $model->firstName]) ?>
-            <?= $form->field($model, 'surname')->textInput(['value' => $model->surname]) ?>
-            <?= $form->field($model, 'regNumber', ['enableAjaxValidation' => true])->textInput(['value' => $model->regNumber]) ?>
-            <?= $form->field($model, 'email')->textInput(['readonly' => true, 'value' => $model->email]) ?>
-            <?= $form->field($model, 'sex')->dropDownList(['Male', 'Female', 'Other']); ?>
-            <div class="form-group">
-                <?= Html::submitButton('Save', ['class' => 'btn btn-primary', 'id' => 'btn-submit-profile']) ?>
+            <div class="panel panel-default">
+                <div class="panel-heading">Personal Details</div>
+                <div class="panel-body">
+                    <?= $form->field($model, 'firstName')->textInput(['autofocus' => true, 'value' => $model->firstName]) ?>
+                    <?= $form->field($model, 'surname')->textInput(['value' => $model->surname]) ?>
+                    <?= $form->field($model, 'regNumber', ['enableAjaxValidation' => true])->textInput(['value' => $model->regNumber]) ?>
+                    <?= $form->field($model, 'email')->textInput([
+                        'class' => 'cursor-disabled form-control',
+                        'readonly' => true,
+                        'value' => $model->email])
+                    ?>
+                    <?= $form->field($model, 'sex')->dropDownList(['Male', 'Female', 'Other']); ?>
+                    <div class="form-group">
+                        <?= Html::submitButton('Save', ['class' => 'btn btn-primary', 'id' => 'btn-submit-profile']) ?>
+                    </div>
+                </div>
             </div>
             <?php ActiveForm::end(); ?>
         </div>
