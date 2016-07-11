162ef4db663d3755fa4e3363aa6eeda0de30503e
nkmathew <kipkoechmathew@gmail.com>
2016-07-11 14:49:21 +0300

    Create logbook table

---
+++ migrations/m160711_114250_create_logbook.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation for table `logbook`.
+ */
+class m160711_114250_create_logbook extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('logbook', [
+            'id' => $this->primaryKey(),
+            'text' => $this->string(),
+            'updated' => $this->string()->notNull()->unique(),
+            'created' => $this->string(),
+            'author' => $this->string(),
+        ], $tableOptions);
+
+        $this->addForeignKey('FK_logbook_user', 'logbook', 'author', 'user', 'email');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        // drops foreign key for table `profile`
+        $this->dropForeignKey(
+            'FK_logbook_user',
+            'logbook'
+        );
+
+        $this->dropTable('logbook');
+    }
+}
