434903d785006385af572cef364c36710ed39274
nkmathew <kipkoechmathew@gmail.com>
2016-07-19 11:49:33 +0300

    Ajax validation kills contact form submission

--- views/site/contact.php
+++ views/site/contact.php
@@ -20,7 +20,7 @@ $this->params['breadcrumbs'][] = $this->title;
     </p>
     <div class="row">
         <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'contact-form', 'enableAjaxValidation' => true]); ?>
+            <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
                 <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
                 <?= $form->field($model, 'email') ?>
                 <?= $form->field($model, 'subject') ?>
