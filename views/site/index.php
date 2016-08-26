a8b35a059049a63ff76d549ac39bcc9309a8acac
nkmathew <kipkoechmathew@gmail.com>
2016-08-26 05:17:06 +0300

    Disable all tabs until the user sets the internship duration first

--- views/site/index.php
+++ views/site/index.php
@@ -3,10 +3,15 @@
 /* @var $this yii\web\View */
 
 use app\models\ProfileForm;
+use app\models\Profile;
 use yii\bootstrap\Tabs;
 use app\controllers\SiteController;
 
 $this->title = 'Home';
+
+$email = Yii::$app->user->identity->email;
+$profile = Profile::findByEmail($email);
+
 ?>
 
 <title>Home</title> <!-- For Pjax's sake -->
@@ -17,27 +22,33 @@ $this->title = 'Home';
             'label' => '<span class="glyphicon glyphicon-user"></span> Profile',
             'content' => $this->context->actionProfile(true),
             'headerOptions' => ['id' => 'profile-tab', 'class' => 'tab-main'],
-        ],
-        [
-            'label' => '<span class="glyphicon glyphicon-book"></span> Log Book',
-            'content' => $this->render('logbook'),
-            'headerOptions' => ['id' => 'logbook-tab', 'class' => 'tab-main'],
-            'options' => ['id' => 'tab-logbook'],
-        ],
-        [
-            'label' => '<span class="glyphicon glyphicon-stats"></span> Progress',
-            'content' => $this->context->actionProgress(),
-            'headerOptions' => ['id' => 'progress-tab', 'class' => 'tab-main'],
-            'options' => ['id' => 'tab-progress'],
-        ],
-        [
-            'label' => '<span class="glyphicon glyphicon-list-alt"></span> Preview',
-            'content' => $this->render('preview'),
-            'headerOptions' => ['id' => 'preview-tab', 'class' => 'tab-main'],
-            'options' => ['id' => 'tab-preview'],
         ]
     ];
-    $email = Yii::$app->user->identity->email;
+    if ($profile->duration) {
+        $moreItems = [
+            [
+                'label' => '<span class="glyphicon glyphicon-book"></span> Log Book',
+                'content' => $this->render('logbook'),
+                'headerOptions' => ['id' => 'logbook-tab', 'class' => 'tab-main'],
+                'options' => ['id' => 'tab-logbook'],
+            ],
+            [
+                'label' => '<span class="glyphicon glyphicon-stats"></span> Progress',
+                'content' => $this->context->actionProgress(),
+                'headerOptions' => ['id' => 'progress-tab', 'class' => 'tab-main'],
+                'options' => ['id' => 'tab-progress'],
+            ],
+            [
+                'label' => '<span class="glyphicon glyphicon-list-alt"></span> Preview',
+                'content' => $this->render('preview'),
+                'headerOptions' => ['id' => 'preview-tab', 'class' => 'tab-main'],
+                'options' => ['id' => 'tab-preview'],
+            ]
+        ];
+        $tabItems = array_merge_recursive($moreItems, $tabItems);
+    } else {
+        Yii::$app->session->setFlash('error', 'Please save the start date and duration of your internship first');
+    }
     if (!Yii::$app->user->isGuest && !strchr($email, 'student')) {
         $tabItems[] = [
             'label' => '<span class="glyphicon glyphicon-console"></span> Coordinator\'s Console',
