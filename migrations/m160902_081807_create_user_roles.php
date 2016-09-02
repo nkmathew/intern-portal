43805da5ea3227d347751ad60d7fa9a7be887042
nkmathew <kipkoechmathew@gmail.com>
2016-09-02 18:16:39 +0300

    Fix: table being created with MyISAM engine

--- migrations/m160902_081807_create_user_roles.php
+++ migrations/m160902_081807_create_user_roles.php
@@ -12,11 +12,17 @@ class m160902_081807_create_user_roles extends Migration
      */
     public function up()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
         $this->createTable('user_roles', [
             'id' => $this->primaryKey(),
             'role_name' => $this->string()->unique(),
             'description' => $this->string()
-        ]);
+        ], $tableOptions);
 
         $this->batchInsert('user_roles',
             ['role_name', 'description'],
