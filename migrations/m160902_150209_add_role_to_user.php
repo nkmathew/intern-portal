84c063fe232aa089fa4debdb62f7d8ca270eaab6
nkmathew <kipkoechmathew@gmail.com>
2016-09-28 21:47:44 +0300

    Initialize admin account on first run with default password of test123

--- migrations/m160902_150209_add_role_to_user.php
+++ migrations/m160902_150209_add_role_to_user.php
@@ -14,6 +14,15 @@ class m160902_150209_add_role_to_user extends Migration
     {
         $this->addColumn('user', 'role', $this->string());
         $this->addForeignKey('FK_user_roles_user', 'user', 'role', 'user_roles', 'role_name');
+
+        $this->insert('user', [
+            'auth_key' => '',
+            'password_hash' => '$2y$13$EgZmVsH0taLeKwrDR4k49e838RZFqytucF.nDcBFEYKwDKL9sAxMW', // Default password: test123
+            'email' => 'admin@admin.com',
+            'created_at' => time(),
+            'updated_at' => time(),
+            'role' => 'superuser',
+        ]);
     }
 
     /**
